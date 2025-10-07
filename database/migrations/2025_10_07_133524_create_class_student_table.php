8ad846d233c123f961c61be2404049de9dd68d2e
theFather8 <maaizwajeeh@gmail.com>
2025-10-07 18:37:28 +0500

    create class student table and migrate

---
+++ database/migrations/2025_10_07_133524_create_class_student_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('class_student', function (Blueprint $table) {
+            $table->foreignId('class_id')->constrained()->onDelete('cascade');
+            $table->foreignId('student_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+
+            // Prevents a student from being assigned to the same class multiple times
+            $table->primary(['class_id', 'student_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('class_student');
+    }
+};
